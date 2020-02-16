8af7e4d5749746976911b91737cefe928482ad66
AmakawaYuki <amakawa.yuki@prevent.co.jp>
2020-02-16 16:10:30 +0900

    add: vue-svg-loaderの追加と設定

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   chainWebpack: config => {
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
     config.module
       .rule("pug")
       .test(/\.pug$/)
       .use("pug-plain-loader")
       .loader("pug-plain-loader")
       .end();
+    svgRule
+      .use("babel-loader")
+      .loader("babel-loader")
+      .end()
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader");
   },
   css: {
     loaderOptions: {
