4838328e49ed655880b1ee0f8745c838155d05cb
AmakawaYuki <amakawa.yuki@prevent.co.jp>
2020-03-17 22:58:25 +0900

    add: google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIAPSvcGIJkVJJu0XOK-oAhlrlzUvsAUQ",
+  authDomain: "recipe-formatter.firebaseapp.com",
+  databaseURL: "https://recipe-formatter.firebaseio.com",
+  projectId: "recipe-formatter",
+  storageBucket: "recipe-formatter.appspot.com",
+  messagingSenderId: "672439336459",
+  appId: "1:672439336459:web:165db6e166d1605f86510e",
+  measurementId: "G-3CR7GS7K15"
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
