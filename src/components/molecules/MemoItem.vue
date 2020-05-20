d2fc6dcc8f1276e355a8b4074b4f9b5f5bddeecc
AmakawaYuki <amakawa.yuki@prevent.co.jp>
2020-05-20 23:49:44 +0900

    add: メモの並び替えの切り替え

--- src/components/molecules/MemoItem.vue
+++ src/components/molecules/MemoItem.vue
@@ -1,11 +1,6 @@
 <template lang="pug">
   .MemoItem
-    div.flex
-      .icon.wrap-icons.flex
-        i.move-icon(v-if="!isFirst")
-          up-icon(@click="onUpItem")
-        i.move-icon(v-if="!isLast")
-          down-icon(@click="onDownItem")
+    div.flex(v-if="isEdit")
       span.index {{index+1}}.
       .wrap-forms.flex
         app-textarea.description(
@@ -13,7 +8,16 @@
           v-model="memo.description"
           rowNum=3
         )
-      i.icon(v-if="!isFirst")
+    div.flex(v-else)
+      .icon.wrap-icons.flex
+        i.move-icon(v-show="!isFirst")
+          up-icon(@click="onUpItem")
+        i.move-icon(v-show="!isLast")
+          down-icon(@click="onDownItem")
+      span.index {{index+1}}.
+      .wrap-texts
+        p {{memo.description}}
+      i.icon(v-show="!isFirst")
           close-icon(@click="onDeleteItem")
 </template>
 <script lang="ts">
@@ -40,7 +44,8 @@ export default Vue.extend({
   props: {
     memo: Object as PropType<Memo>,
     index: Number,
-    isLast: Boolean
+    isLast: Boolean,
+    isEdit: Boolean
   },
   computed: {
     isFirst(): boolean {
@@ -63,23 +68,29 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .MemoItem {
   width: 100%;
-  .wrap-icons {
-    width: 31px;
-  }
   .index {
     width: 15px;
     font-size: 12px;
   }
   .wrap-forms {
-    width: calc(100% - 70px);
+    width: 100%;
     textarea {
       width: 100%;
     }
   }
-  .move-icon {
-    svg {
-      width: 14px;
-      height: 20px;
+  .wrap-texts {
+    width: calc(100% - 71px);
+    textarea {
+      width: 100%;
+    }
+  }
+  .wrap-icons {
+    width: 31px;
+    .move-icon {
+      svg {
+        width: 14px;
+        height: 20px;
+      }
     }
   }
 }
